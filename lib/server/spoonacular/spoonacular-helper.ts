6a6d29af4c38d0322cd7a56dba79d33d81c7cf7f
dsessoms <dsessoms94@gmail.com>
2025-12-22 23:26:49 -0800

    feat: added spoonacular recipe search to helper

--- lib/server/spoonacular/spoonacular-helper.ts
+++ lib/server/spoonacular/spoonacular-helper.ts
@@ -392,6 +392,214 @@ export class Spoonacular {
 		return this.makeApiCall("recipes/random");
 	}
 
+	/**
+	 * Search for recipes using a variety of filters and constraints
+	 * @param params - Search parameters
+	 * @param params.query - The (natural language) recipe search query
+	 * @param params.cuisine - The cuisine(s) of the recipes. One or more, comma separated (will be interpreted as 'OR')
+	 * @param params.excludeCuisine - The cuisine(s) the recipes must not match. One or more, comma separated (will be interpreted as 'AND')
+	 * @param params.diet - The diet(s) for which the recipes must be suitable. Comma means AND, pipe | means OR. Example: "gluten free,vegetarian" or "vegan|vegetarian"
+	 * @param params.intolerances - A comma-separated list of intolerances. All recipes returned must not contain ingredients that are not suitable for people with the intolerances entered
+	 * @param params.equipment - The equipment required. Multiple values will be interpreted as 'or'. Example: "blender, frying pan, bowl"
+	 * @param params.includeIngredients - A comma-separated list of ingredients that should/must be used in the recipes
+	 * @param params.excludeIngredients - A comma-separated list of ingredients or ingredient types that the recipes must not contain
+	 * @param params.type - The type of recipe (e.g., "main course", "dessert", "appetizer")
+	 * @param params.instructionsRequired - Whether the recipes must have instructions
+	 * @param params.fillIngredients - Add information about the ingredients and whether they are used or missing in relation to the query
+	 * @param params.addRecipeInformation - If set to true, you get more information about the recipes returned
+	 * @param params.addRecipeInstructions - If set to true, you get analyzed instructions for each recipe returned
+	 * @param params.addRecipeNutrition - If set to true, you get nutritional information about each recipe returned
+	 * @param params.author - The username of the recipe author
+	 * @param params.tags - User defined tags that have to match. The author param has to be set
+	 * @param params.recipeBoxId - The id of the recipe box to which the search should be limited to
+	 * @param params.titleMatch - Enter text that must be found in the title of the recipes
+	 * @param params.maxReadyTime - The maximum time in minutes it should take to prepare and cook the recipe
+	 * @param params.minServings - The minimum amount of servings the recipe is for
+	 * @param params.maxServings - The maximum amount of servings the recipe is for
+	 * @param params.ignorePantry - Whether to ignore typical pantry items, such as water, salt, flour, etc.
+	 * @param params.sort - The strategy to sort recipes by (e.g., "calories", "time", "popularity")
+	 * @param params.sortDirection - The direction in which to sort. Must be either 'asc' (ascending) or 'desc' (descending)
+	 * @param params.minCarbs - The minimum amount of carbohydrates in grams the recipe must have per serving
+	 * @param params.maxCarbs - The maximum amount of carbohydrates in grams the recipe can have per serving
+	 * @param params.minProtein - The minimum amount of protein in grams the recipe must have per serving
+	 * @param params.maxProtein - The maximum amount of protein in grams the recipe can have per serving
+	 * @param params.minCalories - The minimum amount of calories the recipe must have per serving
+	 * @param params.maxCalories - The maximum amount of calories the recipe can have per serving
+	 * @param params.minFat - The minimum amount of fat in grams the recipe must have per serving
+	 * @param params.maxFat - The maximum amount of fat in grams the recipe can have per serving
+	 * @param params.minAlcohol - The minimum amount of alcohol in grams the recipe must have per serving
+	 * @param params.maxAlcohol - The maximum amount of alcohol in grams the recipe can have per serving
+	 * @param params.minCaffeine - The minimum amount of caffeine in milligrams the recipe must have per serving
+	 * @param params.maxCaffeine - The maximum amount of caffeine in milligrams the recipe can have per serving
+	 * @param params.minCopper - The minimum amount of copper in milligrams the recipe must have per serving
+	 * @param params.maxCopper - The maximum amount of copper in milligrams the recipe can have per serving
+	 * @param params.minCalcium - The minimum amount of calcium in milligrams the recipe must have per serving
+	 * @param params.maxCalcium - The maximum amount of calcium in milligrams the recipe can have per serving
+	 * @param params.minCholine - The minimum amount of choline in milligrams the recipe must have per serving
+	 * @param params.maxCholine - The maximum amount of choline in milligrams the recipe can have per serving
+	 * @param params.minCholesterol - The minimum amount of cholesterol in milligrams the recipe must have per serving
+	 * @param params.maxCholesterol - The maximum amount of cholesterol in milligrams the recipe can have per serving
+	 * @param params.minFluoride - The minimum amount of fluoride in milligrams the recipe must have per serving
+	 * @param params.maxFluoride - The maximum amount of fluoride in milligrams the recipe can have per serving
+	 * @param params.minSaturatedFat - The minimum amount of saturated fat in grams the recipe must have per serving
+	 * @param params.maxSaturatedFat - The maximum amount of saturated fat in grams the recipe can have per serving
+	 * @param params.minVitaminA - The minimum amount of Vitamin A in IU the recipe must have per serving
+	 * @param params.maxVitaminA - The maximum amount of Vitamin A in IU the recipe can have per serving
+	 * @param params.minVitaminC - The minimum amount of Vitamin C in milligrams the recipe must have per serving
+	 * @param params.maxVitaminC - The maximum amount of Vitamin C in milligrams the recipe can have per serving
+	 * @param params.minVitaminD - The minimum amount of Vitamin D in micrograms the recipe must have per serving
+	 * @param params.maxVitaminD - The maximum amount of Vitamin D in micrograms the recipe can have per serving
+	 * @param params.minVitaminE - The minimum amount of Vitamin E in milligrams the recipe must have per serving
+	 * @param params.maxVitaminE - The maximum amount of Vitamin E in milligrams the recipe can have per serving
+	 * @param params.minVitaminK - The minimum amount of Vitamin K in micrograms the recipe must have per serving
+	 * @param params.maxVitaminK - The maximum amount of Vitamin K in micrograms the recipe can have per serving
+	 * @param params.minVitaminB1 - The minimum amount of Vitamin B1 in milligrams the recipe must have per serving
+	 * @param params.maxVitaminB1 - The maximum amount of Vitamin B1 in milligrams the recipe can have per serving
+	 * @param params.minVitaminB2 - The minimum amount of Vitamin B2 in milligrams the recipe must have per serving
+	 * @param params.maxVitaminB2 - The maximum amount of Vitamin B2 in milligrams the recipe can have per serving
+	 * @param params.minVitaminB5 - The minimum amount of Vitamin B5 in milligrams the recipe must have per serving
+	 * @param params.maxVitaminB5 - The maximum amount of Vitamin B5 in milligrams the recipe can have per serving
+	 * @param params.minVitaminB3 - The minimum amount of Vitamin B3 in milligrams the recipe must have per serving
+	 * @param params.maxVitaminB3 - The maximum amount of Vitamin B3 in milligrams the recipe can have per serving
+	 * @param params.minVitaminB6 - The minimum amount of Vitamin B6 in milligrams the recipe must have per serving
+	 * @param params.maxVitaminB6 - The maximum amount of Vitamin B6 in milligrams the recipe can have per serving
+	 * @param params.minVitaminB12 - The minimum amount of Vitamin B12 in micrograms the recipe must have per serving
+	 * @param params.maxVitaminB12 - The maximum amount of Vitamin B12 in micrograms the recipe can have per serving
+	 * @param params.minFiber - The minimum amount of fiber in grams the recipe must have per serving
+	 * @param params.maxFiber - The maximum amount of fiber in grams the recipe can have per serving
+	 * @param params.minFolate - The minimum amount of folate in micrograms the recipe must have per serving
+	 * @param params.maxFolate - The maximum amount of folate in micrograms the recipe can have per serving
+	 * @param params.minFolicAcid - The minimum amount of folic acid in micrograms the recipe must have per serving
+	 * @param params.maxFolicAcid - The maximum amount of folic acid in micrograms the recipe can have per serving
+	 * @param params.minIodine - The minimum amount of iodine in micrograms the recipe must have per serving
+	 * @param params.maxIodine - The maximum amount of iodine in micrograms the recipe can have per serving
+	 * @param params.minIron - The minimum amount of iron in milligrams the recipe must have per serving
+	 * @param params.maxIron - The maximum amount of iron in milligrams the recipe can have per serving
+	 * @param params.minMagnesium - The minimum amount of magnesium in milligrams the recipe must have per serving
+	 * @param params.maxMagnesium - The maximum amount of magnesium in milligrams the recipe can have per serving
+	 * @param params.minManganese - The minimum amount of manganese in milligrams the recipe must have per serving
+	 * @param params.maxManganese - The maximum amount of manganese in milligrams the recipe can have per serving
+	 * @param params.minPhosphorus - The minimum amount of phosphorus in milligrams the recipe must have per serving
+	 * @param params.maxPhosphorus - The maximum amount of phosphorus in milligrams the recipe can have per serving
+	 * @param params.minPotassium - The minimum amount of potassium in milligrams the recipe must have per serving
+	 * @param params.maxPotassium - The maximum amount of potassium in milligrams the recipe can have per serving
+	 * @param params.minSelenium - The minimum amount of selenium in micrograms the recipe must have per serving
+	 * @param params.maxSelenium - The maximum amount of selenium in micrograms the recipe can have per serving
+	 * @param params.minSodium - The minimum amount of sodium in milligrams the recipe must have per serving
+	 * @param params.maxSodium - The maximum amount of sodium in milligrams the recipe can have per serving
+	 * @param params.minSugar - The minimum amount of sugar in grams the recipe must have per serving
+	 * @param params.maxSugar - The maximum amount of sugar in grams the recipe can have per serving
+	 * @param params.minZinc - The minimum amount of zinc in milligrams the recipe must have per serving
+	 * @param params.maxZinc - The maximum amount of zinc in milligrams the recipe can have per serving
+	 * @param params.offset - The number of results to skip (between 0 and 900)
+	 * @param params.number - The number of expected results (between 1 and 100)
+	 * @returns Promise resolving to the search results
+	 */
+	static searchRecipes(params: {
+		query?: string;
+		cuisine?: string;
+		excludeCuisine?: string;
+		diet?: string;
+		intolerances?: string;
+		equipment?: string;
+		includeIngredients?: string;
+		excludeIngredients?: string;
+		type?: string;
+		instructionsRequired?: boolean;
+		fillIngredients?: boolean;
+		addRecipeInformation?: boolean;
+		addRecipeInstructions?: boolean;
+		author?: string;
+		tags?: string;
+		recipeBoxId?: number;
+		titleMatch?: string;
+		maxReadyTime?: number;
+		minServings?: number;
+		maxServings?: number;
+		ignorePantry?: boolean;
+		sort?: string;
+		sortDirection?: string;
+		minCarbs?: number;
+		maxCarbs?: number;
+		minProtein?: number;
+		maxProtein?: number;
+		minCalories?: number;
+		maxCalories?: number;
+		minFat?: number;
+		maxFat?: number;
+		minAlcohol?: number;
+		maxAlcohol?: number;
+		minCaffeine?: number;
+		maxCaffeine?: number;
+		minCopper?: number;
+		maxCopper?: number;
+		minCalcium?: number;
+		maxCalcium?: number;
+		minCholine?: number;
+		maxCholine?: number;
+		minCholesterol?: number;
+		maxCholesterol?: number;
+		minFluoride?: number;
+		maxFluoride?: number;
+		minSaturatedFat?: number;
+		maxSaturatedFat?: number;
+		minVitaminA?: number;
+		maxVitaminA?: number;
+		minVitaminC?: number;
+		maxVitaminC?: number;
+		minVitaminD?: number;
+		maxVitaminD?: number;
+		minVitaminE?: number;
+		maxVitaminE?: number;
+		minVitaminK?: number;
+		maxVitaminK?: number;
+		minVitaminB1?: number;
+		maxVitaminB1?: number;
+		minVitaminB2?: number;
+		maxVitaminB2?: number;
+		minVitaminB5?: number;
+		maxVitaminB5?: number;
+		minVitaminB3?: number;
+		maxVitaminB3?: number;
+		minVitaminB6?: number;
+		maxVitaminB6?: number;
+		minVitaminB12?: number;
+		maxVitaminB12?: number;
+		minFiber?: number;
+		maxFiber?: number;
+		minFolate?: number;
+		maxFolate?: number;
+		minFolicAcid?: number;
+		maxFolicAcid?: number;
+		minIodine?: number;
+		maxIodine?: number;
+		minIron?: number;
+		maxIron?: number;
+		minMagnesium?: number;
+		maxMagnesium?: number;
+		minManganese?: number;
+		maxManganese?: number;
+		minPhosphorus?: number;
+		maxPhosphorus?: number;
+		minPotassium?: number;
+		maxPotassium?: number;
+		minSelenium?: number;
+		maxSelenium?: number;
+		minSodium?: number;
+		maxSodium?: number;
+		minSugar?: number;
+		maxSugar?: number;
+		minZinc?: number;
+		maxZinc?: number;
+		offset?: number;
+		number?: number;
+	}): Promise<Response> {
+		return this.makeApiCall("recipes/complexSearch", {
+			...params,
+			addRecipeNutrition: true,
+		});
+	}
+
 	static parseRecipeFromWebsite(params: {
 		url: string;
 	}): Promise<SpoonacularRecipeResponse> {
